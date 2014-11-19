35fd1dc191c8826bf1e693c3e0c4bc3df76fcef4
mhkeller <michael.keller@gmail.com>
2014-11-19 10:35:47 -0500

    unschedule

---
+++ bin/unschedule-prompts.js
@@ -0,0 +1,13 @@
+var path = require('path');
+
+var home_dir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
+		config_path = home_dir + '/.conf/kestrel-config.json';
+
+var config = require(config_path),
+		repo_name = path.basename(path.resolve('./'));
+
+module.exports = {
+  "bucket_environment": prompt("Unschedule from `staging` or `prod`?", "staging"),
+  "trigger": prompt("Enter the sync trigger"),
+  "local_path": prompt("Unschedule local directory... e.g. `" + repo_name + "/output`", repo_name)
+ }
